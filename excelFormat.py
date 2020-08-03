fff55108d8a65e50615cd62b441eff7866e219dd
Qin <junfengx.qin@intel.com>
2020-08-03 15:10:55 +0800

    update

--- excelFormat.py
+++ excelFormat.py
@@ -4,30 +4,23 @@ import json
 import re
 import os
 import datetime
-import profile
-
-
-'''
-[text:'BHCPBZJSA0001', text:'1#搅拌更换搅拌减速机作业', text:'成品工区', text:'一线混料槽上方', text:'/', text:'扳手、撬棍、吊车', text:'使用工具拆卸固定连接螺栓，拆掉旧减速机，回装新减速机', text:'设备停运、断
-电、挂牌', text:'触电伤害', text:'1.1正确佩戴好绝缘手套和绝缘鞋。\n1.2相关流程有效隔离。\n1.3使用正确有效的工器具。', empty:'']
-'''
+import httpx
+import time
 
 # 读取.xls文件
-
-
 def readExcel(filename):
     f = xlrd.open_workbook(filename=filename)
     data = f.sheet_by_index(0)
     return data
 
+
 # 填充数据
-
-
 def fill(data):
     global tmp
     dataList = []
     tmp = ''
     tmp2 = ''
+    sheet = data.name
     for line in data.get_rows():
         l = line[0].value
         try:
@@ -43,13 +36,13 @@ def fill(data):
         else:
             tmpLine[0] = tmp
             tmpLine[4] = tmp2
+        tmpLine.append(sheet)
         dataList.append(tmpLine)
     return dataList
 
 
-# 生成数据列表，[{},{}]  ---> 这是基础数据，左右的数据操作都在这个上面做
-
-def generateDict(datalist):
+# 生成数据列表，[{},{}]  ---> 这是基础数据，所有的数据操作都在这个上面做
+def generateDict(datalist, orgcode):
     dataDict = {}
     for i in datalist:
         if i[0] not in dataDict.keys():
@@ -61,12 +54,12 @@ def generateDict(datalist):
                 "relaSafeCode": i[9],
                 "relaSafeDesc": i[9]
             }
-            dataDict[i[0]] = [i[0], i[1], "orgCode", i[4],
-                              i[4], i[5], i[5], i[2], i[3], i[6], [tmpDict]]
+            dataDict[i[0]] = [i[0], i[1], orgcode, i[4],
+                              i[4], i[5], i[5], i[-1], i[3], i[6], [tmpDict]]
         else:
             n = dataDict[i[0]][-1][-1]["workNo"]
             tmpDict2 = {
-                "workNo": n+1,
+                "workNo": n + 1,
                 "workDesc": '|'.join(str(i[7]).split('、')),
                 "relaRiskCode": i[8],
                 "relaRiskName": i[8],
@@ -81,7 +74,6 @@ def generateDict(datalist):
 
 
 # 转换成 json
-
 def dictToJson(datadict, code1, code2, code3, code4):
     tmpList = []
     for v in datadict.values():
@@ -92,7 +84,7 @@ def dictToJson(datadict, code1, code2, code3, code4):
             tmpDict["orgCode"] = v[2]
             tmpDict["relaMaterielCode"] = '|'.join(
                 [code1[i] if i != "/" else "" for i in v[3].split('、')])
-            tmpDict["relaMaterielName"] = v[3] if v[3]!= "/" else ""
+            tmpDict["relaMaterielName"] = v[3] if v[3] != "/" else ""
             tmpDict["relaToolCode"] = '|'.join(
                 [code2[i] for i in v[5].split('、')])
             tmpDict["relaToolName"] = '|'.join(v[6].split('、'))
@@ -105,7 +97,8 @@ def dictToJson(datadict, code1, code2, code3, code4):
         # 替换 relaRiskCode，relaSafeCode  现在是个列表
         for i in v[-1]:
             # print(i["relaRiskName"])
-            i["relaRiskCode"] = '|'.join([code3[j] for j in (re.sub(r'\d|\.', '', i) for i in i["relaRiskName"].split('\n')) if j])
+            i["relaRiskCode"] = '|'.join(
+                [code3[j] for j in (re.sub(r'\d|\.', '', i) for i in i["relaRiskName"].split('\n')) if j])
             i["relaRiskName"] = '|'.join(
                 [re.sub(r'\d|\.', '', i) for i in i["relaRiskName"].split('\n')])
             i["relaSafeCode"] = '|'.join([code4[j] for j in (
@@ -125,7 +118,7 @@ def generateDictCode(datadict, index):
     tmpList = []
     tmpDict = {}
     for v in datadict.values():
-        tmpList += [i if i != "/" else ""for i in v[index].split("、")]
+        tmpList += [i if i != "/" else "" for i in v[index].split("、")]
         # try:
         #     tmpList += [i if i != "/" else ""for i in v[index].split("、")]
         # except Exception:
@@ -136,9 +129,8 @@ def generateDictCode(datadict, index):
         tmpDict[i] = k
     return tmpDict
 
+
 # 生成编码字典  --> 处理字典中的列表 relaRiskName
-
-
 def generateDictCode2(datadict, fieldname):
     tmpList = []
     tmpDict = {}
@@ -168,11 +160,30 @@ def scanDir(path):
     return tmpList
 
 
+# [[]]
+# 双列表数据处理
+
+def twoList(tlist, dict):
+    tmpList = []
+    tmp1 = [i.split("|") for i in tlist]
+    for i in tmp1:
+        for j in i:
+            tmpList.append(dict[j])
+    return "|".join(tmpList)
+
+# 准备工具时被绊倒摔伤|准备工器具时手被挤伤，脚被砸伤|车辆伤害
+
+def twoList2(tlist, dict):
+    tmpList = []
+    for i in tlist.split("|"):
+        if i:
+            tmpList.append(dict[i])
+    return "|".join(tmpList)
+
+
+
 # 处理风险数据关系
-
-
-def extracData(datadict, dict1, dict2):
-    print(dict1)
+def extracData(orgcode, datadict, dict1, dict2):
     tmpList = []
     gList = []
     for v in datadict.values():
@@ -180,6 +191,8 @@ def extracData(datadict, dict1, dict2):
             tmpDict = {}
             relaRiskName = i["relaRiskName"].split("\n")
             relaSafeDesc = i["relaSafeDesc"].split("\n")
+            # print(relaRiskName)
+            # print(relaSafeDesc)
             if len(relaRiskName) < 2:
                 k = re.sub(r'\d|\.', '', relaRiskName[0])
                 if k:
@@ -194,10 +207,17 @@ def extracData(datadict, dict1, dict2):
     for i in tmpList:
         for k, v in i.items():
             tDict = {}
-            tDict["riskCode"] = dict1[k]
-            tDict["riskName"] = k
-            tDict["relaSafeCode"] = '|'.join([dict2[i] for i in v])
-            tDict["relaSafeDesc"] = '|'.join([i for i in v])
+            tmp = twoList2(k, dict1)
+            tDict["RISK_CODE"] = tmp
+            tDict["RISK_NAME"] = k
+            tmp1 = twoList(v, dict2)
+            tDict["RELA_SAFE_CODE"] = tmp1
+            tmp2 = '|'.join([i for i in v])
+            tDict["RELA_SAFE_DESC"] = tmp2
+            tDict["ORG_CODE"] = orgcode
+            tDict["DR"] = 0
+            tDict["DATA_VERSION"] = 1
+            tDict["DATA_NO"] = hash('{}{}{}{}{}'.format(tmp, k, tmp1, tmp2, orgcode))
             gList.append(tDict)
     return gList
 
@@ -212,15 +232,45 @@ def checkFileFormat(path):
             print('大哥呀文件格式好像有问题啊，检查下吧！！！ --->>{}'.format(filename))
 
 
+# 请求数据
+def post(url, data):
+    http = httpx.post(url=url, data=data)
+    return http.status_code
+
+
+# 获取路径中最后一层文件名
+def fastdir(path):
+    _, tDir = os.path.split(path)
+    return tDir
+
+
+# 生成工器具请求报文数据
+def genFormat(orgcode, codedict):
+
+    tmpList = []
+    for k, v in codedict.items():
+        tmpDict = {
+            "TOOL_CODE": k,
+            "TOOL_NAME": v,
+            "ORG_CODE": orgcode,
+            "DR": 0,
+            "DATA_VERSION": 1,
+            "DATA_NO": hash('{}{}{}'.format(k, v, orgcode)),
+        }
+        tmpList.append(tmpDict)
+    return tmpList
+
+
 def main():
-    path = r"D:\learn\tmp\包化"
+    path = r"C:\Users\qinjunfx\OneDrive - Intel Corporation\Desktop\works\scripts"
+    orgcode = fastdir(path)
     gendata = []
     fileList = scanDir(path)
     for filename in fileList:
         print('扫描文件---->>> {}'.format(filename))
         data = readExcel(filename)
         gendata += fill(data)
-    gddata = generateDict(gendata)
+    gddata = generateDict(gendata, orgcode)
 
     # 工器具数据格式
     code1 = generateDictCode(gddata, 6)
@@ -232,26 +282,30 @@ def main():
     code4 = generateDictCode2(gddata, "relaSafeDesc")
 
     gdd = dictToJson(gddata, code1, code2, code3, code4)
-    # for i in gdd:
-    #     print("写入数据········")
+
+    #   ===========================================最大的那个json===============================================
+    l = len(gdd)
+    for i in range(int(l/10)):
+        dataList = json.dumps(gdd[i*10:(i*10)+10])
+        data = {
+            "boName": "BO_EU_DEF_TOOL",
+            "uid:": "admin",
+            "recordDatas": dataList,
+        }
+        post(r'http://127.0.0.1:8080/bd/jsa/batch/save', data)
+
+    #   ===========================================风险请求报文===============================================
+    # for i in extracData(orgcode, gddata, code3, code4):
+        # print(json.dumps(i))
+    #   ===========================================工器具数据格式===============================================
+    # for i in code1:
     #     print(json.dumps(i))
-    with open('1.txt', 'w') as f:
-        for i in gdd:
-            f.write(json.dumps(i))
-            f.write("\n")
-
-    #  风险格式数据
-    # with open('1.txt', 'w') as f:
-    #     for i in extracData(gddata, code3, code4 ):
-    #         print(i)
-    #         f.write(json.dumps(i))
-    #         f.write("\n")
-
-    # with open('1.txt', 'w') as f:
-    #     for i in code3:
-    #         print(i)
-    #         f.write(json.dumps(i))
-
+    #   ===========================================物料数据格式===============================================
+    # for ii in code2:
+    #     print(json.dumps(ii))
+    #   ===========================================安全措施数据格式===============================================
+    # for iii in code4:
+    #     print(json.dumps(iii))
 
 if __name__ == "__main__":
     print(datetime.datetime.now())
