035a86080b0b98a39b57ffa7bc4b3ff3795c2eec
qinjunfx <junfengx.qin@intel.com>
2020-11-16 16:45:54 +0800

    添加文档

--- mhxy.py
+++ mhxy.py
@@ -3,10 +3,19 @@ Author: xianxiaoyin
 LastEditors: xianxiaoyin
 Descripttion: 
 Date: 2020-10-30 14:16:49
-LastEditTime: 2020-11-12 23:02:22
+LastEditTime: 2020-11-16 16:45:11
 '''
 oleview download
 
 http://timgolden.me.uk/pywin32-docs/html/com/win32com/readme.htm
 
 http://timgolden.me.uk/pywin32-docs/html/com/win32com/HTML/QuickStartClientCom.html
+
+
+https://my.oschina.net/zxcholmes/blog/484789
+
+https://docs.microsoft.com/en-us/previous-versions/d0kh9f4c(v=vs.140)
+
+
+this:
+https://blog.csdn.net/lpwmm/article/details/83720318
