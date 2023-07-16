aae5c620329bae55cd03268c534dde3866a579ca
xianxiaoyin <525607894@qq.com>
2023-07-16 21:56:11 +0800

    格式化代码

--- exce.py
+++ exce.py
@@ -1,16 +1,18 @@
-'''
+"""
 Author: xianxiaoyin
 LastEditors: xianxiaoyin
 Descripttion: 先将xls文件转成xlsx的文件，然后格式化数据，然后再将xlsx文件转成xls [设置行高，宽度之类]
 Date: 2020-11-09 22:30:26
 LastEditTime: 2020-11-16 21:13:27
-'''
+"""
 import os
 import sys
-from openpyxl import load_workbook
-from openpyxl.utils import get_column_letter, column_index_from_string
-import win32com.client as win32
 import zipfile
+
+import win32com.client as win32
+from openpyxl import load_workbook
+from openpyxl.utils import column_index_from_string, get_column_letter
+
 
 # 解压zip
 def unzip(filename):
@@ -20,6 +22,7 @@ def unzip(filename):
             path, _ = os.path.split(filename)
             zf.extractall(path)
 
+
 # 处理中文乱码的文件名
 
 
@@ -27,16 +30,14 @@ def changeFilename(path):
     for filename in os.listdir(path):
         # if os.path.isdir(os.path.join(path, filename)):
         try:
-            zip_file = filename.encode('cp437').decode('gbk')
+            zip_file = filename.encode("cp437").decode("gbk")
         except:
-            zip_file = filename.encode('utf-8').decode('utf-8')
+            zip_file = filename.encode("utf-8").decode("utf-8")
         if os.path.isdir(os.path.join(path, filename)):
-            os.rename(os.path.join(path, filename),
-                      os.path.join(path, zip_file))
+            os.rename(os.path.join(path, filename), os.path.join(path, zip_file))
             changeFilename(os.path.join(path, zip_file))
         else:
-            os.rename(os.path.join(path, filename),
-                      os.path.join(path, zip_file))
+            os.rename(os.path.join(path, filename), os.path.join(path, zip_file))
 
 
 def get_files(path, file_list=[]):
@@ -57,7 +58,7 @@ def xlsToxlsx(filename, formats):
         new_filename = "{}x".format(filename)
     elif formats == 56:  # .xlsx-->.xls
         pass
-    excel = win32.gencache.EnsureDispatch('Excel.Application')
+    excel = win32.gencache.EnsureDispatch("Excel.Application")
     wb = excel.Workbooks.Open(filename)
     wb.SaveAs(new_filename, FileFormat=formats)
     wb.Close()
@@ -69,7 +70,13 @@ def checkDataFormat(ws):
     cell_list = []
     row_list = []
 
-    tag = ["yyyy/m/d;@", "[$-F800]dddd\,\ mmmm\ dd\,\ yyyy", "mm-dd-yy", 'm"月"d"日";@', 'yyyy"年"m"月"d"日";@']
+    tag = [
+        "yyyy/m/d;@",
+        "[$-F800]dddd\,\ mmmm\ dd\,\ yyyy",
+        "mm-dd-yy",
+        'm"月"d"日";@',
+        'yyyy"年"m"月"d"日";@',
+    ]
     for row in ws.rows:
         for cell in row:
             # print(cell.value)
@@ -92,17 +99,17 @@ def formatExcel(filename, filename2, height=13, width=15):
         ws = wb[wb.sheetnames[index]]
 
         cdf, cdf2 = checkDataFormat(ws)
-        for i in range(1, ws.max_row+1):
+        for i in range(1, ws.max_row + 1):
             if i in cdf2:
-                ws.row_dimensions[i].height = height+4
+                ws.row_dimensions[i].height = height + 4
             else:
                 ws.row_dimensions[i].height = height
 
-        for j in range(1, ws.max_column+1):
+        for j in range(1, ws.max_column + 1):
             if get_column_letter(j) in cdf:
                 ws.column_dimensions[get_column_letter(j)].width = width
             else:
-                ws.column_dimensions[get_column_letter(j)].width = width-3
+                ws.column_dimensions[get_column_letter(j)].width = width - 3
 
     wb.save(filename2)
 
@@ -114,10 +121,10 @@ def main():
     changeFilename(os.getcwd())
     for filename in get_files(os.getcwd()):
         xlsToxlsx(filename, 51)
-        xlsxFileName = "{}x".format(filename)
+        xlsxFileName = f"{filename}x"
         path, fname = os.path.split(xlsxFileName)
         tmpPath = path.split(os.sep)
-        index =  len(tmpPath) - len(os.getcwd().split(os.sep))
+        index = len(tmpPath) - len(os.getcwd().split(os.sep))
         tmpPath.insert(-index, "new")
         newPath = str(os.sep).join(tmpPath)
         if not os.path.exists(newPath):
@@ -126,5 +133,5 @@ def main():
         formatExcel(xlsxFileName, newXlsxFfileName, 12)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- loggingtest.py
+++ loggingtest.py
@@ -1,16 +1,8 @@
-'''
-Author: xianxiaoyin
-LastEditors: xianxiaoyin
-Descripttion: 
-Date: 2020-09-15 21:44:35
-LastEditTime: 2020-09-15 21:45:01
-'''
-# coding utf-8
-
 import logging
 import logging.config
 
 logging.config.fileConfig("logging.conf")
 logger = logging.getLogger(__name__)
 
-logger.info('1111111111')
+
+logger.info("1111111111")
